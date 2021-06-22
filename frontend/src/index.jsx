aa694d85dc58192e8a939632ff8da7121e2d872b
RillianDiello <rilliansf@gmail.com>
2021-06-21 22:38:46 -0400

    Instalando e instanciando o Redux para genrenciar os estados da aplicação

--- frontend/src/index.jsx
+++ frontend/src/index.jsx
@@ -1,8 +1,21 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
+import { applyMiddleware,createStore } from 'redux'
+import { Provider } from 'react-redux'
+
+import promise from 'redux-promise'
+
 import App from './main/app'
+import reducers from './main/reducers'
+const devTools = window.__REDUX_DEVTOOLS_EXTENSION__ &&
+    window.__REDUX_DEVTOOLS_EXTENSION__()
+    //São tres chamadas em constante
+const store = applyMiddleware(promise)(createStore)(reducers, devTools) // esse metodo cria a store com os reducers que deixamos staticos
 
 ReactDOM.render(
-    <App />,
+    <Provider store={store}>
+        <App />
+    </Provider>,
+
     document.getElementById('app')
 )
